50bc9ce727a288b94297b3b2f4de7f09dbd0bd68
Dominic Betts <1454644+dominicbetts@users.noreply.github.com>
2022-10-17 11:56:34 +0100

    Edge 1.4 deployment

---
+++ edge-vm-deploy-1-4/README.md
@@ -0,0 +1,5 @@
+# iotedge-vm-deploy - Release 1.4
+
+For detailed instructions to use the contents of this folder, see https://learn.microsoft.com/azure/iot-central/core/tutorial-connect-iot-edge-device
+
+The Azure Resource Manager template used her is based on: https://github.com/Azure/iotedge-vm-deploy/blob/1.4/README.md. It has been modified to use DPS to connect to IoT Central.
---
+++ edge-vm-deploy-1-4/genoneline.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+'''genonline.py - Python 3 version of AnHowe's script to package a cloud-init.txt script into an
+   Azure Resource Manage template format. 
+   goal: "commandToExecute": "[variables('jumpboxWindowsCustomScript')]
+"'''
+import os
+import re
+import sys
+
+
+def convertToOneArmTemplateLine(file):
+    with open(file) as f:
+        content = f.read()
+
+    # convert to one line
+    content = content.replace("\\", "\\\\")
+    content = content.replace("\r\n", "\\n")
+    content = content.replace("\n", "\\n")
+    content = content.replace('"', '\\"')
+
+    # replace {{{ }}} with variable names
+    return re.sub(r"{{{([^}]*)}}}", r"',variables('\1'),'", content)
+
+def convertToOneAzureBicepLine(file):
+    with open(file) as f:
+        content = f.read()
+
+    # convert to one line
+    content = content.replace("\\", "\\\\")
+    content = content.replace("\r\n", "\\n")
+    content = content.replace("\n", "\\n")
+    content = content.replace("\t", "\\t")
+    content = content.replace("'", "\\'")
+
+    # replace {{{ }}} with variable names
+    return re.sub(r"{{{([^}]*)}}}", r"${\1}", content)
+
+
+def usage():
+    print('    usage: ', os.path.basename(sys.argv[0]), 'file1')
+    print('    builds a one line string to send to commandToExecute')
+
+
+def main():
+    if len(sys.argv) != 2:
+        usage()
+        sys.exit(1)
+
+    file = sys.argv[1]
+    if not os.path.exists(file):
+        sys.exit('Error: file: ' + file + ' does not exist')
+
+    # build the yml file for cluster
+    onelineArm = convertToOneArmTemplateLine(file)
+
+    print('Syntax: ARM template:')
+    print('----------------------------')
+    print('"customData": "[base64(concat(\'' + onelineArm + '\'))]\",')
+    print()
+
+    onelineBicep = convertToOneAzureBicepLine(file)
+    print()
+    print('Syntax: Azure Bicep')
+    print('-------------------')
+    print('customData: base64(\'' + onelineBicep + '\')')
+
+if __name__ == "__main__":
+    main()
